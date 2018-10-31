c76037656770734a9dc2bc54a87f34a086820897
zhangjie <731255090@qq.com>
2018-11-01 00:18:43 +0800

    权限验证服务器

---
+++ ticketAuthentication/src/main/java/com/ticket/TicketAuthenticationApplication.java
@@ -0,0 +1,11 @@
+package com.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketAuthenticationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TicketAuthenticationApplication.class, args);
+    }
+}
